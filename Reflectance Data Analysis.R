7efc2c26f79c70c642af5cbf1ed9f4eebd619fd5
rashikaranasinghe <44656113+rashikaranasinghe@users.noreply.github.com>
2024-09-17 13:07:58 -0700

    Add files via upload

---
+++ Reflectance Data Analysis.R
@@ -0,0 +1,11 @@
+#####################################################################
+## Reflectance_data_analysis_Asian_Flamebacks_script.R
+# started by Rashika W. Ranasinghe 17 Sept 2024
+
+# This file includes R scripts for analysis reflectance spectrometric data () used in the manuscript titled: "*********".
+
+
+############################################################################
+################## 1.  Generate SNPs-based PCA ############################
+############################################################################
+
